50a2e6525e3a45e59bcdf1f92baaef12fa6d4743
joe-arbo <joearbo13@vt.edu>
2022-02-19 03:17:07 -0500

    Updated and Cleaned Sensor Code

--- sensors/IEEE_Sensors.ino
+++ sensors/IEEE_Sensors.ino
@@ -45,33 +45,49 @@ float duration, distance1, distance2, distance3, distance4, distance5, distance6
 double calculatePD(double error_now, double error_last) {
   //P(error_now) + D(error_now-error_last)
 
-  double transError = calculateTranslationalError(((distance7 + distance8) / 2), ((distance3 + distance4) / 2), 6);
+  //double transError = calculateTranslationalError(((distance7 + distance8) / 2), ((distance3 + distance4) / 2), 6);
   //Serial.print("Trans Error: ");
   //Serial.println(transError);
 
-  //transError pos: move right
-  //transError neg: move left
   double transThreshold = 1.5;
   double avgRight = (distance7 + distance8) / 2;
   double avgLeft = (distance3 + distance4) / 2;
-  //if (transError > transThreshold){
+  translationalCorrection(avgRight, avgLeft, transThreshold);
+  
+
+  //double rotError = calculateRotationalError(distance7, distance8, 0);
+  double rotDiff = distance7 - distance8;
+  double rotThreshold = 1.0;
+  rotationalCorrection(rotDiff, rotThreshold);
+}
+
+//Temporary? simple implementation
+void translationalCorrection(double avgRight, double avgLeft, double transThreshold) {
   if((avgRight > avgLeft) && (avgRight - avgLeft) > transThreshold){
     Serial.println("Move Right");
-    //calculateWheelSpeed
+    //calculateWheelSpeed?
   } else
-  //if (transError < (-1 * transThreshold)){
   if((avgLeft > avgRight) && (avgLeft - avgRight) > transThreshold){
     Serial.println("Move Left");
-    //calculateWheelSpeed
+    //calculateWheelSpeed?
   }
-  //double rotError = calculateRotationalError(distance7, distance8, 0);
-  
+}
+
+//Temporary? simple implementation
+void rotationalCorrection(double rotDiff, double rotThreshold) {
+  if (rotDiff > rotThreshold) {
+    Serial.println("Turn counter clockwise");
+  } else
+  if (rotDiff < (-1 * rotThreshold)) {
+    Serial.println("Turn clockwise");
+  }
 }
 
 double calculateWheelSpeed(double T_Base, double translational_correction, double rotational_correction) {
   
 }
 
+/*
 double calculateTranslationalError(double avgLeft, double avgRight, double desiredDist) {
   //get average of sensor distances first
   //average - desired
@@ -87,20 +103,8 @@ double calculateTranslationalError(double avgLeft, double avgRight, double desir
 
 double calculateRotationalError(double distSensor1, double distSensor2, double desiredAngle) {
   //right triangle math goes here
-  int threshold = 1;
-
-  //if this is pos: turn counter clockwise
-  //if this is neg: turn clockwise
-  int diff = distSensor1 - distSensor2;
-  
-  while (diff > threshold) {
-    Serial.println("Turn counter clockwise: decrease wheel speed of left wheels");
-  }
-  while (diff < (-1 * threshold)) {
-    Serial.println("Turn clockwise: decrease wheel speed of right wheels");
-  }
-  return 0.0; 
 }
+*/
 
 void setup() {
   // put your setup code here, to run once:
@@ -120,112 +124,81 @@ void loop() {
   distance8 = sonar8.ping_cm();
   
   // Send results to Serial Monitor
-  
+  //SENSOR 1
   Serial.print("Distance1 = ");
-  
-  if (distance1 >= 400 || distance1 <= 1) 
-  {
+  if (distance1 >= 400 || distance1 <= 1) {
     Serial.println("Out of range");
-  }
-  else 
-  {
+  } else {
     Serial.print(distance1);
     Serial.println(" cm");
   }
+
   // SENSOR 2
   Serial.print("Distance2 = ");
-  
-  if (distance2 >= 400 || distance2 <= 1) 
-  {
+  if (distance2 >= 400 || distance2 <= 1) {
     Serial.println("Out of range");
-  }
-  else 
-  {
+  } else {
     Serial.print(distance2);
     Serial.println(" cm");
   }
 
   // SENSOR 3
   Serial.print("Distance3 = ");
-  
-  if (distance3 >= 400 || distance3 <= 1) 
-  {
+  if (distance3 >= 400 || distance3 <= 1) {
     Serial.println("Out of range");
-  }
-  else 
-  {
+  } else {
     Serial.print(distance3);
     Serial.println(" cm");
   }
 
   // SENSOR 4
   Serial.print("Distance4 = ");
-  
-  if (distance4 >= 400 || distance4 <= 1) 
-  {
+  if (distance4 >= 400 || distance4 <= 1) {
     Serial.println("Out of range");
-  }
-  else 
-  {
+  } else {
     Serial.print(distance4);
     Serial.println(" cm");
   }
 
   // SENSOR 5
   Serial.print("Distance5 = ");
-  
-  if (distance5 >= 400 || distance5 <= 1) 
-  {
+  if (distance5 >= 400 || distance5 <= 1) {
     Serial.println("Out of range");
-  }
-  else 
-  {
+  } else {
     Serial.print(distance5);
     Serial.println(" cm");
   }
 
   // SENSOR 6
   Serial.print("Distance6 = ");
-  
-  if (distance6 >= 400 || distance6 <= 1) 
-  {
+  if (distance6 >= 400 || distance6 <= 1) {
     Serial.println("Out of range");
-  }
-  else 
-  {
+  } else {
     Serial.print(distance6);
     Serial.println(" cm");
   }
 
-  ///////////////// SENSOR 7
+  // SENSOR 7
   Serial.print("Distance7 = ");
-  
-  if (distance7 >= 400 || distance7 <= 1) 
-  {
+  if (distance7 >= 400 || distance7 <= 1) {
     Serial.println("Out of range");
-  }
-  else 
-  {
+  } else {
     Serial.print(distance7);
     Serial.println(" cm");
   }
 
 
-  ///////////// SENSOR 8
+  // SENSOR 8
   Serial.print("Distance8 = ");
-  
-  if (distance8 >= 400 || distance8 <= 1) 
-  {
+  if (distance8 >= 400 || distance8 <= 1) {
     Serial.println("Out of range");
-  }
-  else 
-  {
+  } else {
     Serial.print(distance8);
     Serial.println(" cm");
   }
   
-
-    calculatePD(0,0);
+  //Call Main Calculation Body
+  calculatePD(0,0);
 
   //double Speed_FR, Speed_FL, Speed_RR, Speed_RL;
   //double P, D;
@@ -236,23 +209,6 @@ void loop() {
   //Calculate the error (translational and rotational, depending on state)
   //Calculate the speed per wheel with errors and T_Base
   //output speed, set T_Last to T_E
-  
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-  
   delay(2000);
 }
