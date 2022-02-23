b6426befe0b4f607a9096c7febb455cc9219cf68
wtgrimsley <60411582+wtgrimsley@users.noreply.github.com>
2022-02-23 12:31:56 -0500

    added a main file that implements multithreading

---
+++ main.py
@@ -0,0 +1,48 @@
+import integration
+import threading
+
+def Speaker():
+    for i in range(20):
+        print("Play sound")
+
+def OLED():
+    for i in range(20):
+        print("Display updated")
+
+class OLEDThread(threading.Thread):
+   def __init__(self, name):
+      threading.Thread.__init__(self)
+      self.name = name
+
+   def run(self):
+      print("Starting " + self.name)
+      OLED()
+      print("Exiting "+ self.name)
+
+class SpeakerThread(threading.Thread):
+   def __init__(self, name):
+      threading.Thread.__init__(self)
+      self.name = name
+
+   def run(self):
+      print("Starting " + self.name)
+      Speaker()
+      print("Exiting "+ self.name)
+
+class IntegrationThread(threading.Thread):
+   def __init__(self, name):
+      threading.Thread.__init__(self)
+      self.name = name
+
+   def run(self):
+      print("Starting " + self.name)
+      integration.main()
+      print("Exiting "+ self.name)
+
+thread1 = IntegrationThread("integration thread")
+thread2 = OLEDThread("OLED thread")
+thread3 = SpeakerThread("speaker thread")
+
+thread1.start()
+thread2.start()
+thread3.start()
