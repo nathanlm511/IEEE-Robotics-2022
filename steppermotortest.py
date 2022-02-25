d9c7fea40982c5ab2ecc80f17eabf962230ee294
kathrynr13 <kathrynr@vt.edu>
2022-02-25 15:28:58 -0500

    Update steppermotortest.py

--- steppermotortest.py
+++ steppermotortest.py
@@ -6,7 +6,11 @@ from arm import *
 ser = serial.Serial('/dev/ttyACM0', 9600, timeout=1)
 ser.reset_input_buffer()
 
-STEP1, DIR1 = 12, 6
+# define digital IO pins
+STEP1 = 12 # PWM signal
+
+# direction pins
+DIR1 = 6
 DIR2 = 17
 DIR3 = 16
 DIR4 = 26
@@ -15,7 +19,7 @@ DIR4 = 26
 CW = 1
 CCW = 0
 
-# # Driver Enable Pins (active low)
+# Driver Enable Pins (active low)
 EN1 = 27
 EN2 = 22
 EN3 = 23
@@ -24,15 +28,15 @@ EN4 = 24
 # Set up microstepping
 MODE = (20, 5, 21) # Microstep resolution GPIO pins
 RESOLUTION = {'Full': (0, 0, 0),
-              'Half': (1, 0, 0),
-              '1/4': (0, 1, 0),
-              '1/8': (1, 1, 0),
-              '1/16': (1, 0, 1),
-              '1/32': (1, 0, 1)}
-
+			  'Half': (1, 0, 0),
+			  '1/4': (0, 1, 0),
+			  '1/8': (1, 1, 0),
+			  '1/16': (1, 0, 1),
+			  '1/32': (1, 0, 1)}
 
 # Conect the pigpiod daemon
 pi = pigpio.pi()
+
 
 # Disable all motor drivers
 pi.write(EN1, 1)
@@ -43,7 +47,7 @@ sleep(1)
 
 # set microstep value
 for i in range(3):
-    pi.write(MODE[i], RESOLUTION['1/32'][i])
+	pi.write(MODE[i], RESOLUTION['1/32'][i])
 
 # Enable motor drivers one at a time
 pi.write(EN1, 0)
@@ -54,60 +58,98 @@ pi.write(EN3, 0)
 sleep(1)
 pi.write(EN4, 0)
 
-# frequency = 1000 # 0 (off) to 1-125M
-# duty_cycle = 500000 # 0 (off) to 1M (fully on)
-# 
-# pi.hardware_PWM(STEP1, frequency, duty_cycle)
-print("HERE")
 
 def forwards():
-    frequency = 1000
-    duty_cycle = 500000
-    pi.hardware_PWM(STEP1, frequency, duty_cycle)
+	frequency = 1000
+	duty_cycle = 500000
+	pi.hardware_PWM(STEP1, frequency, duty_cycle)
 
-    pi.write(DIR1, 0)
-    pi.write(DIR2, 1)
-    pi.write(DIR3, 0)
-    pi.write(DIR4, 1)
+	pi.write(DIR1, 0)
+	pi.write(DIR2, 1)
+	pi.write(DIR3, 0)
+	pi.write(DIR4, 1)
 
 def stopMoving():
-    frequency = 0
-    duty_cycle = 0
-    pi.hardware_PWM(STEP1, frequency, duty_cycle)
+	frequency = 0
+	duty_cycle = 0
+	pi.hardware_PWM(STEP1, frequency, duty_cycle)
 
 def clockwise():
-   frequency = 750
-   duty_cycle = 500000
-   pi.hardware_PWM(STEP1, frequency, duty_cycle)
+	frequency = 750
+	duty_cycle = 500000
+	pi.hardware_PWM(STEP1, frequency, duty_cycle)
 
-   pi.write(DIR1, 0)
-   pi.write(DIR2, 0)
-   pi.write(DIR3, 0)
-   pi.write(DIR4, 0)
+	pi.write(DIR1, 0)
+	pi.write(DIR2, 0)
+	pi.write(DIR3, 0)
+	pi.write(DIR4, 0)
+
+def counterClockwise():
+	frequency = 750
+	duty_cycle = 500000
+	pi.hardware_PWM(STEP1, frequency, duty_cycle)
+
+	pi.write(DIR1, 1)
+	pi.write(DIR2, 1)
+	pi.write(DIR3, 1)
+	pi.write(DIR4, 1)
+
+def backwards():
+	frequency = 1000
+	duty_cycle = 500000
+	pi.hardware_PWM(STEP1, frequency, duty_cycle)
+
+	pi.write(DIR1, 0)
+	pi.write(DIR2, 1)
+	pi.write(DIR3, 0)
+	pi.write(DIR4, 1)
+	frequency = 500 # 0 (off) to 1-125M
+	duty_cycle = 0 # 0 (off) to 1M (fully on)
+	pi.hardware_PWM(STEP1, frequency, duty_cycle)
+	# Disable all motor drivers
+	pi.write(EN1, 1)
+	pi.write(EN2, 1)
+	pi.write(EN3, 1)
+	pi.write(EN4, 1)
+	sleep(1)
+	pi.stop()
+
+# turns off motors
+def turnOffMotors():
+	frequency = 500 # 0 (off) to 1-125M
+	duty_cycle = 0 # 0 (off) to 1M (fully on)
+	pi.hardware_PWM(STEP1, frequency, duty_cycle)
+	# Disable all motor drivers
+	pi.write(EN1, 1)
+	pi.write(EN2, 1)
+	pi.write(EN3, 1)
+	pi.write(EN4, 1)
+	sleep(1)
+	pi.stop()
 
 try:
-    while True:
+	while True:
 
-        try:
-            line = ser.readline().decode('utf-8').rstrip()
-            print(line)
-            if line == "F":
-                forwards()
-            elif line == "S":
-                print("STOP!")
-                stopMoving()
-            elif line == "A":
-                print("Executing arm code")
-                arm()
-                launch()
-                deinitialize()
-            elif line == "C":
-                clockwise()
-            elif line == "Fl":
-                ser.reset_input_buffer()
-        except:
-            pass
-        sleep(0.025)
+		try:
+			line = ser.readline().decode('utf-8').rstrip()
+			print(line)
+			if line == "F":
+				forwards()
+			elif line == "S":
+				print("STOP!")
+				stopMoving()
+			elif line == "A":
+				print("Executing arm code")
+				arm()
+				launch()
+				deinitialize()
+			elif line == "C":
+				clockwise()
+			elif line == "Fl":
+				ser.reset_input_buffer()
+		except:
+			pass
+		sleep(0.025)
 #        ser.reset_input_buffer()
 #try:
 #    while True:
@@ -154,15 +196,15 @@ try:
 #        else:
 #            pass
 except KeyboardInterrupt:
-    print('\nCtrl-C pressed. Stopping PIGPIO and exiting...')
+	print('\nCtrl-C pressed. Stopping PIGPIO and exiting...')
 finally:
-    frequency = 500 # 0 (off) to 1-125M
-    duty_cycle = 0 # 0 (off) to 1M (fully on)
-    pi.hardware_PWM(STEP1, frequency, duty_cycle)
-    # Disable all motor drivers
-    pi.write(EN1, 1)
-    pi.write(EN2, 1)
-    pi.write(EN3, 1)
-    pi.write(EN4, 1)
-    sleep(1)
-    pi.stop()
+	frequency = 500 # 0 (off) to 1-125M
+	duty_cycle = 0 # 0 (off) to 1M (fully on)
+	pi.hardware_PWM(STEP1, frequency, duty_cycle)
+	# Disable all motor drivers
+	pi.write(EN1, 1)
+	pi.write(EN2, 1)
+	pi.write(EN3, 1)
+	pi.write(EN4, 1)
+	sleep(1)
+	pi.stop()
