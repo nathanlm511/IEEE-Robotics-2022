f2be795f6b4a8eff8b4734be49d21eae7572572f
wtgrimsley <williamtg@vt.edu>
2022-03-23 14:51:54 -0400

    changed handler for ctrl-c and estop

--- main.py
+++ main.py
@@ -16,33 +16,29 @@ STOP_GPIO = 8
 pi.set_mode(START_GPIO, pigpio.INPUT)
 pi.set_pull_up_down(START_GPIO, pigpio.PUD_UP)
 
+
 def stop_hander():
-   signal.signal(signal.SIGINT)
-
-
-# declare handler for estop
-cb1 = pi.callback(STOP_GPIO, pigpio.EITHER_EDGE, stop_hander)
-
-try:
-   arm.startPosition()
-   #oled.dispVT()
-   
-   # wait for start button
-   buttonNotPressed = True
-   while buttonNotPressed:
-      buttonState = pi.read(START_GPIO)
-      if not buttonState:
-         buttonNotPressed = False
-   
-   print('STARTING INTEGRATION MAIN')
-   integration.main()
-
-except:
    steppermotortest.turnOffMotors()
    integration.stopCamera()
    arm.servosOff()
    print("\nCtrl-C pressed. Stopping PIGPIO and exiting...")
    print('motors off')
 
-# thread1.join()
-# thread2.join()
+
+# declare handler for estop
+signal.signal(signal.SIGINT, stop_hander)
+cb1 = pi.callback(STOP_GPIO, pigpio.EITHER_EDGE, stop_hander)
+
+
+arm.startPosition()
+#oled.dispVT()
+
+# wait for start button
+buttonNotPressed = True
+while buttonNotPressed:
+   buttonState = pi.read(START_GPIO)
+   if not buttonState:
+      buttonNotPressed = False
+
+print('STARTING INTEGRATION MAIN')
+integration.main()
