1bc7bf8499ecd928622d66fe73e82ad4dcabb54a
wtgrimsley <williamtg@vt.edu>
2022-03-23 14:42:42 -0400

    added start position for arm and wait for start button

--- main.py
+++ main.py
@@ -1,16 +1,39 @@
+from tracemalloc import stop
 import integration
-import os
-import time
 import steppermotortest
 import arm
-#import oled
-# import arm
+import pigpio
+# import oled
+# import os
+# import time
 
-start_time = time.time()
+pi = pigpio.pi()
+
+START_GPIO = 7
+STOP_GPIO = 8
+
+pi.set_mode(START_GPIO, pigpio.INPUT)
+pi.set_pull_up_down(START_GPIO, pigpio.PUD_UP)
+
+def stop_hander():
+   pass
+
+
+# declare handler for estop
 
 
 try:
+   arm.startPosition()
    #oled.dispVT()
+   
+   # wait for start button
+   buttonNotPressed = True
+   while buttonNotPressed:
+      buttonState = pi.read(START_GPIO)
+      if not buttonState:
+         buttonNotPressed = False
+   
+   print('STARTING INTEGRATION MAIN')
    integration.main()
 
 except:
